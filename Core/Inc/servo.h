6725701f22e43e6d68db8594c0bf8a833331f59a
HansPan <b09502037@ntu.edu.tw>
2026-05-11 23:01:57 +0800

    hanspan

---
+++ Core/Inc/servo.h
@@ -0,0 +1,18 @@
+/*
+ * servo.h
+ *
+ *  Created on: May 10, 2026
+ *      Author: USER
+ */
+
+#ifndef INC_SERVO_H_
+#define INC_SERVO_H_
+
+#include "main.h"
+#include <stdint.h>
+
+void Servo_Init(void);
+void Servo_Stop(void);
+void Servo_UnlockSequence(void);
+
+#endif /* INC_SERVO_H_ */
---
+++ Core/Src/servo.c
@@ -0,0 +1,51 @@
+/*
+ * servo.c
+ *
+ *  Created on: May 10, 2026
+ *      Author: USER
+ */
+#include "servo.h"
+
+extern TIM_HandleTypeDef htim2;
+
+#define SERVO_TIMER      htim2
+#define SERVO_CHANNEL    TIM_CHANNEL_1
+
+#define SERVO_STOP_US    1500
+#define SERVO_UNLOCK_US  1700
+#define SERVO_LOCK_US    1300
+
+#define UNLOCK_TIME_MS   600
+#define HOLD_TIME_MS     3000
+#define LOCK_TIME_MS     600
+
+static void Servo_SetPulse(uint16_t pulse_us)
+{
+    __HAL_TIM_SET_COMPARE(&SERVO_TIMER, SERVO_CHANNEL, pulse_us);
+}
+
+void Servo_Init(void)
+{
+    HAL_TIM_PWM_Start(&SERVO_TIMER, SERVO_CHANNEL);
+    Servo_Stop();
+}
+
+void Servo_Stop(void)
+{
+    Servo_SetPulse(SERVO_STOP_US);
+}
+
+void Servo_UnlockSequence(void)
+{
+    Servo_SetPulse(SERVO_UNLOCK_US);
+    HAL_Delay(UNLOCK_TIME_MS);
+
+    Servo_Stop();
+    HAL_Delay(HOLD_TIME_MS);
+
+    Servo_SetPulse(SERVO_LOCK_US);
+    HAL_Delay(LOCK_TIME_MS);
+
+    Servo_Stop();
+}
+
